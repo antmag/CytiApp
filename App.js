73067359a5c9c6e5ba5fca90b7caa93abf2eeadd
Unknown <maguet.antoine@hotmail.fr>
2017-12-20 14:57:04 +0100

    Login facebook

--- App.js
+++ App.js
@@ -9,6 +9,15 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 
 import BottomBar from './components/bottomBar/BottomBar';
 
+import FBLoginView from './components/loginPage/LoginPage';
+
+import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
+
+var LoginBehavior = {
+  'ios': FBLoginManager.LoginBehaviors.Browser,
+  'android': FBLoginManager.LoginBehaviors.Native
+}
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -30,8 +39,24 @@ export default class App extends Component<{}> {
       //     {instructions}
       //   </Text>
       // </View>
+      //<View style={{flex:1}}>
+        //<BottomBar />
+      //</View>
+
       <View style={{flex:1}}>
-        <BottomBar />
+        <Text style={styles.welcome}>Welcome to CYTI App</Text>
+        <FBLogin
+            buttonView={<FBLoginView/>}
+            ref={(fbLogin) => { this.fbLogin = fbLogin }}
+            loginBehavior={LoginBehavior[Platform.OS]}
+            permissions={["email","user_friends"]}
+            onLogin={function(e){console.log(e)}}
+            onLoginFound={function(e){console.log(e)}}
+            onLoginNotFound={function(e){console.log(e)}}
+            onLogout={function(e){console.log(e)}}
+            onCancel={function(e){console.log(e)}}
+            onPermissionsMissing={function(e){console.log(e)}}
+        />
       </View>
     );
   }
--- android/app/src/main/java/com/cytiapp/MainApplication.java
+++ android/app/src/main/java/com/cytiapp/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+//import com.centaurwarchief.facebooklogin.FacebookLoginPackage;
+import com.magus.fblogin.FacebookLoginPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,8 +23,10 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+      return Arrays.<ReactPackage>asList( 
+          new MainReactPackage(),
+          new FacebookLoginPackage()
+          
       );
     }
 
