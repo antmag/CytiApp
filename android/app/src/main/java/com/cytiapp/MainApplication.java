7ad0f165f4ed46505d8f5611944380590d376b11
Unknown <maguet.antoine@hotmail.fr>
2018-01-08 16:33:55 +0100

    working changes for cadeau

--- android/app/src/main/java/com/cytiapp/MainApplication.java
+++ android/app/src/main/java/com/cytiapp/MainApplication.java
@@ -30,7 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
             new PhotoViewPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
